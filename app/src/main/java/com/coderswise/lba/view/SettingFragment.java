dc4ae654c04c1bca82dd42701561edca3b7428af
Coders Wise <coderswise@gmail.com>
2018-04-07 10:05:54 +0600

    added alarm Service and broadcast receiver

--- app/src/main/java/com/coderswise/lba/view/SettingFragment.java
+++ app/src/main/java/com/coderswise/lba/view/SettingFragment.java
@@ -19,6 +19,8 @@ public class SettingFragment extends Fragment {
 
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
